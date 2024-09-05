a7ab86346b5ab24edc684a441e452e133e03dbbd
Joshua Thijssen <jthijssen@noxlogic.nl>
2024-09-05 09:47:37 +0200

    added version.json (proxy)

--- app/routers/default.py
+++ app/routers/default.py
@@ -9,13 +9,13 @@ router = APIRouter()
 
 # https://www.patorjk.com/software/taag/#p=display&f=Doom&t=Skeleton
 LOGO = r"""
-___  ___     _            _       _        
-|  \/  |    | |          | |     | |       
-| .  . | ___| |_ __ _  __| | __ _| |_ __ _ 
+___  ___     _            _       _
+|  \/  |    | |          | |     | |
+| .  . | ___| |_ __ _  __| | __ _| |_ __ _
 | |\/| |/ _ \ __/ _` |/ _` |/ _` | __/ _` |
 | |  | |  __/ || (_| | (_| | (_| | || (_| |
 \_|  |_/\___|\__\__,_|\__,_|\__,_|\__\__,_|
-                                           
+
 """
 
 
@@ -30,5 +30,17 @@ def index() -> Response:
     except BaseException as e:
         content += "\nNo version information found"
         logger.info("Version info could not be loaded: %s" % e)
+
+    return Response(content)
+
+
+@router.get("/version.json")
+def version_json() -> Response:
+    try:
+        with open(Path(__file__).parent.parent.parent / 'version.json', 'r') as file:
+            content = file.read()
+    except BaseException as e:
+        logger.info("Version info could not be loaded: %s" % e)
+        return Response(status_code=404)
 
     return Response(content)
